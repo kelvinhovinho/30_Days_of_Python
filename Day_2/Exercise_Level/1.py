1618d53576e76e7c25d297e88f662abe6fa76ce5
kelvinhovinho <kelvinadamba@hotmail.com>
2022-07-30 15:16:37 +0300

    finished exercise_1 on day_2

---
+++ Day_2/Exercise_Level/1.py
@@ -0,0 +1,11 @@
+# Day 2:30 Days of python
+
+firt_name = 'Kelvin'
+lasr_name = 'Adamba'
+full_name = 'Kelvin Adamba Odiaga'
+
+country = 'Kenya'
+city = "Nairobi"
+age = 27
+year = 2022
+is_married = False
