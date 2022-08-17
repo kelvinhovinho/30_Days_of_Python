a9b367b43b3c3ce73883a229f4e58282ee175fd7
kelvinhovinho <kelvinadamba@hotmail.com>
2022-08-17 19:26:45 +0300

    wrote a program that calcuares thenarea of a triangle in day 3 exercise

---
+++ Day_3/Exercise/area_Triangls.py
@@ -0,0 +1,9 @@
+#calculating area of a triangle getting user inpiut of height and width
+#Area = 0.5*b*h
+
+width = int(input('what is the height of the triangle \n'))
+base = int(input('what is the base of the triangle \n'))
+
+area = (width*base*0.5)
+
+print(f'The area of the triangle is {area}')
