ff329f259d53acccc154745889a86d382cd3740f
kelvinhovinho <kelvinadamba@hotmail.com>
2022-08-26 07:54:01 +0300

    finished execise on day 4

---
+++ Day_3/Exercise/operator.py
@@ -0,0 +1,6 @@
+# Use and operator to check if 'on' is found in both 'python' and 'dragon
+words=input('enter a word\n')
+w= 'on'
+
+print(w in words)
+
---
+++ Day_4/main.py
@@ -0,0 +1,36 @@
+a = 'Thirty'
+b='Days'
+c = 'Of'
+d='Python'
+
+print(a+' '+b+' '+c+' '+c+' '+d+'.')
+
+company = 'Coding For All'
+
+print(company)
+print(len(company))
+print(company.upper())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company[1::])
+print('Coding' in company)
+print(company.index('All'))
+print(company.replace('Coding','Python'))
+print(company.rindex('l'))
+print(company.index('Coding'))
+print(company.replace(' ', ''))
+
+python_libraries = ['Django','Flask','Bottle','Pyramis','Falcon']
+
+print(' '.join(python_libraries))
+
+print('I am enjoying this challenge \nI just wonder what is next')
+
+print('Name\tAge\tCountry\tCity')
+print('Asabeneh\t250\tFinland\tHelsinki')
+
+radius = 10
+area = 3.14*radius**2
+
+print(f'The ares of a circle with radius {radius} is {area} metres aquare')
