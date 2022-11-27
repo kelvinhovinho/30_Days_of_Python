b09eee02ecc27bc252a29ed409b72da9f735a7fb
Akelvino <vinho892@gmail.com>
2022-11-27 22:58:05 +0300

    working on day 11

--- Day_11/exercise.py
+++
@@ -1,9 +0,0 @@
-def sum_of_two_numbers(a,b):
-    return a+b
-print(sum_of_two_numbers(5,5))
-
-#area of a circle
-
-def are_of_a_circle(r):
-    return 3.14*r*r 
-print(are_of_a_circle(7))
--- Day_11/main.py
+++ Day_11/main.py
@@ -1,42 +1,27 @@
-# a function is a block of code or a programming statement designed to perfom a certain task
-# it uses the def key word. the function is executed only when the fuction is called or invoked.
+# A function is a reusable block of code or a statent designed to perfom a certain task 
+# when we make a functon we call it declairing a function, wehn we use it we call calling or invoking a function 
 
-import code
-from pyexpat.errors import codes
+# syntax
+
+# def name(parameters):
+    # Code
+    # pass
+#function name
+# name()
 
 
-def function_name():
-    codes
-    codes
-function_name()
+def full_name():
+    fname = 'Kelvin'
+    lname = 'Adamba'
+    space = ' '
+    name = fname + space + lname
+    print(name)
+full_name()
 
-# function without parameters
-
-def generate_full_name():
-    fname = 'kelvin'
-    lname = 'adamba'
-    full_name = fname+' ' + lname
-    print(full_name)
-generate_full_name()
 
 def add_two_numbers():
-    num_one = 2
-    num_two = 3
-    total = num_one+num_two
-    print(total)
+    num1 = 10
+    num2 = 20
+    sum = num1 + num2
+    print(sum)
 add_two_numbers()
-
-def add_number():
-    one = 10
-    two = 5
-    total = one + two
-    return total
-print(add_number())
-
-def fname():
-    firstname = 'kelvin'
-    lastname = 'adamba'
-    space = ' '
-    name = firstname +space + lastname
-    return name
-print(fname())
