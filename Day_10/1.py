7a41eb73c82c40cae725461060a80f3509d43478
kelvinhovinho <59760989+kelvinhovinho@users.noreply.github.com>
2023-10-20 19:39:17 +0300

    Delete Day_10 directory

--- Day_10/1.py
+++
@@ -1,8 +0,0 @@
-number =0
-
-while number <=10:
-    print(number)
-    number+=1
-
-for num in range(10):
-    print(num+1)
--- Day_10/level_3.py
+++
@@ -1,6 +0,0 @@
-fruits = ['banana','orange','mango','lemon']
-rev =[]
-for i in range(3,-1,-1):
-    rev.append(fruits[i])
-print(rev)
-
